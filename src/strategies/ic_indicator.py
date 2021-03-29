d4bc05829c39103c3168fce9103aae8d08ce4cd0
Mohammad Sabir Khan <mohsabkha@gmail.com>
2021-03-29 05:12:02 -0500

    fix to ic strat

--- src/strategies/ic_indicator.py
+++ src/strategies/ic_indicator.py
@@ -10,8 +10,8 @@ def ic_indicator(ticker_values, ic_data):
     #lead span b is the 52 period high + the 52 period low / 2
     ic_data['lead_B'].append((ticker_values['high'].rolling(window=52).max() + 
                               ticker_values['low'].rolling(window=52).min())/2)
-    #the lagging span is the 
-    ic_data['lag_span'].append(ticker_values['close'][-26])
+    #the lagging span is the past
+    ic_data['lag_span'].append(ticker_values['close'][-1])
     #series of checks
     if(len(ic_data['lead_A']) > 53):
         #conversion line is greater than base line in the present
@@ -22,10 +22,11 @@ def ic_indicator(ticker_values, ic_data):
         #ticker values close higher than the cloud in the present
         flag4 = ticker_values['close'] > ic_data['lead_A'][-26]
         flag5 = ticker_values['close'] > ic_data['lead_B'][-26]
+        flag6 = ticker_values['close'] < ic_data['lead_B'][-26] or ticker_values['close'] < ic_data['lead_A'][-26]
         #the cloud is green in the future
-        flag6 = ic_data['lead_A'][-1] > ic_data['lead_B'][-1]
+        flag7 = ic_data['lead_A'][-1] > ic_data['lead_B'][-1]
         #check if all conditions are true in order to buy
-        if(flag1 and flag2 and flag3 and flag4 and flag5 and flag6):
+        if(flag1 and flag2 and flag3 and flag4 and flag5 and flag6 and flag7):
             # TO-DO : set profit loss/gain here and return it
             return True
         else:
