7575f4492d6af50ef60dbb9860a3c1b9a57d22c1
Gonzalo <gnpantoja@outlook.com>
2021-04-14 00:16:03 -0500

    Heroku build

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-#make sure you create this file based on config_example.py
+# make sure you create this file based on config_example.py
 from config import *
 
 from screener.retreive_data.ticker_functions.get_tickers import get_tickers
@@ -21,75 +21,85 @@ import time
 from polygon import WebSocketClient, STOCKS_CLUSTER
 import os
 
+
 def main():
-    ######################################################### Phase 1 - get tickers
+    # Phase 1 - get tickers
     try:
-        #get the current time
+        # get the current time
         now = datetime.now().strftime('%H:%M:%S')
         print(':::::::::::::::::::START TIME IS', now)
-        #check current directory
+        # check current directory
         dir_path = os.getcwd()
         path = (dir_path + TICKER_PAGE_PATH)
         dir = os.listdir(path)
-        #if the path does not have ticker files, get ticker files
+        # if the path does not have ticker files, get ticker files
         if(len(dir) == 0):
             get_tickers()
-        #use combine tickers to ensure all ticker data from get_tickers is correct    
+        # use combine tickers to ensure all ticker data from get_tickers is correct
         datetime.now().strftime('%H:%M:%S')
-        print(':::::::::::::::::::ENTERING COMBINE TICKERS -',datetime.now().strftime('%H:%M:%S'))
-        #store ticker data into symbols
+        print(':::::::::::::::::::ENTERING COMBINE TICKERS -',
+              datetime.now().strftime('%H:%M:%S'))
+        # store ticker data into symbols
         symbols = combine_tickers('polygon_data/tickers')
-        #combine tickers will create csv files, read those csv files into polygon_tickers with 
-        #symbols variable as backup
+        # combine tickers will create csv files, read those csv files into polygon_tickers with
+        # symbols variable as backup
         polygon_tickers = pd.read_csv('polygon_tickers.csv')
-        #set dataframe index as the ticker
+        # set dataframe index as the ticker
         polygon_tickers.set_index('ticker', inplace=True)
-        #remove duplicates of the tickers
+        # remove duplicates of the tickers
         polygon_tickers.drop_duplicates()
-        #filter out tickers that are not bought and sold on common us exchanges and reassign them to 
-        #symbols variable
-        print(':::::::::::::::::::ENTERING FILTER US STOCKS',datetime.now().strftime('%H:%M:%S'))
+        # filter out tickers that are not bought and sold on common us exchanges and reassign them to
+        # symbols variable
+        print(':::::::::::::::::::ENTERING FILTER US STOCKS',
+              datetime.now().strftime('%H:%M:%S'))
         symbols = filter_us_stocks(polygon_tickers)
     except:
         print('error log: Error In Phase 1 ------------------------------------------')
 
-
-    ######################################################### Phase 2 - get data
+    # Phase 2 - get data
     try:
-        #returns end of day data for symbols
-        print(':::::::::::::::::::ENTERING GET OPEN CLOSE',datetime.now().strftime('%H:%M:%S'))
+        # returns end of day data for symbols
+        print(':::::::::::::::::::ENTERING GET OPEN CLOSE',
+              datetime.now().strftime('%H:%M:%S'))
         data = get_open_close(symbols)
-        #returns a list of symbols that met criteria for day trades
-        print(':::::::::::::::::::DATA BEING SENT TO FIRST DATA FILTER',datetime.now().strftime('%H:%M:%S'))
-        print(':::::::::::::::::::This is the length of that data: ',len(data))
-        print(':::::::::::::::::::ENTERING FIRST DATA FILTER -',datetime.now().strftime('%H:%M:%S'))
+        # returns a list of symbols that met criteria for day trades
+        print(':::::::::::::::::::DATA BEING SENT TO FIRST DATA FILTER',
+              datetime.now().strftime('%H:%M:%S'))
+        print(':::::::::::::::::::This is the length of that data: ', len(data))
+        print(':::::::::::::::::::ENTERING FIRST DATA FILTER -',
+              datetime.now().strftime('%H:%M:%S'))
         filtered_data = first_data_filter(data)
-        #retrieves minute data for symbols that met criteria
-        print(':::::::::::::::::::ENTERING GET MINUTE BARS -',datetime.now().strftime('%H:%M:%S'))
+        # retrieves minute data for symbols that met criteria
+        print(':::::::::::::::::::ENTERING GET MINUTE BARS -',
+              datetime.now().strftime('%H:%M:%S'))
         minute_data = get_minute_bars(first_filter=filtered_data, date=END)
         almost_clean_data = pd.DataFrame(minute_data)
-        almost_clean_data = almost_clean_data.reindex(index=almost_clean_data.index[::-1])
-        #use daily open close rates for each ticker and convert it to dataframe
+        almost_clean_data = almost_clean_data.reindex(
+            index=almost_clean_data.index[::-1])
+        # use daily open close rates for each ticker and convert it to dataframe
         daily_data = pd.DataFrame(data)
         daily_data.set_index('symbol', inplace=True)
-        #filter remaining data again
-        print(':::::::::::::::::::ENTERING SECOND DATA FILTER -', datetime.now().strftime('%H:%M:%S'))
-        recommendation_list = second_data_filter(filtered_data, almost_clean_data, daily_data)
+        # filter remaining data again
+        print(':::::::::::::::::::ENTERING SECOND DATA FILTER -',
+              datetime.now().strftime('%H:%M:%S'))
+        recommendation_list = second_data_filter(
+            filtered_data, almost_clean_data, daily_data)
         recommendation_list = pd.DataFrame(recommendation_list)
-        #print date to ensure that correct date is being used
+        # print date to ensure that correct date is being used
         print(':::::::::::::::::::The following data is being taken from this date: ', END)
     except:
         print('error log: Error In Phase 2 ------------------------------------------')
 
-    ######################################################### Phase 3 - organize filtered data and conduct initial buys
-    #sort data by certain values
+    # Phase 3 - organize filtered data and conduct initial buys
+    # sort data by certain values
     try:
-        my_volume_list_best = recommendation_list.sort_values(by='Volume').tail(5)
+        my_volume_list_best = recommendation_list.sort_values(
+            by='Volume').tail(5)
         my_score_list = recommendation_list.sort_values(by='Score').head(5)
-        #print time to see how long it took to complete process
+        # print time to see how long it took to complete process
         now = datetime.now().strftime('%H:%M:%S')
         print(':::::::::::::::::::END TIME IS ', now)
-        #remove combined tickers data
+        # remove combined tickers data
         os.remove("polygon_tickers_us.csv")
         del symbols
         del polygon_tickers
@@ -108,57 +118,60 @@ def main():
         print('\n\n\n:::::::::::::::::::Top Stocks Based On Highest Volume')
         print(my_volume_list_best)
         print('\n\n\n:::::::::::::::::::CONDTIONING DATA FOR ALPACA')
-        #format data for alpaca that will be called in the next phase
+        # format data for alpaca that will be called in the next phase
         alpaca_top_stocks_array = condition_data(my_score_list)
-        #buy all stocks
+        # buy all stocks
         print(':::::::::::::::::::BUYING STOCKS FOR EXPERIMENT 1')
         ps_gonzalo = PurchaseStock(
             APCA_API_KEY_ID_GONZALO,
-            APCA_API_SECRET_KEY_GONZALO, 
-            APCA_API_BASE_URL_PAPER_GONZALO, 
+            APCA_API_SECRET_KEY_GONZALO,
+            APCA_API_BASE_URL_PAPER_GONZALO,
             alpaca_top_stocks_array,
             BUY_LIMIT_GONZALO)
         ps_gonzalo.run()
-        #buy one stock
+        # buy one stock
         print(':::::::::::::::::::BUYING STOCKS FOR EXPERIMENT 2')
         to_list = [alpaca_top_stocks_array[0]]
         ps_sam = PurchaseStock(
             APCA_API_KEY_ID_SAM,
             APCA_API_SECRET_KEY_SAM,
-            APCA_API_BASE_URL_PAPER_SAM, 
-            to_list, 
+            APCA_API_BASE_URL_PAPER_SAM,
+            to_list,
             BUY_LIMIT_SAM)
         ps_sam.run()
     except:
         print('error log: Error In Phase 3 ------------------------------------------')
-    ######################################################### Phase 4 - get live data and feed into strat for experiments 3-5
+    # Phase 4 - get live data and feed into strat for experiments 3-5
     try:
         print(':::::::::::::::::::CONDTIONING DATA FOR WEBSOCKET')
         stock_score = my_score_list.set_index('Stock')
-        websocket_symbols, websocket_ticker_data, ic_data = strat_list(stock_score)
-        #function to be used by websocket
+        websocket_symbols, websocket_ticker_data, ic_data = strat_list(
+            stock_score)
+        # function to be used by websocket
+
         def entry_to_strats(message):
             strats(message, websocket_ticker_data, ic_data)
-        #begin opening websocket
+        # begin opening websocket
         print(':::::::::::::::::::OPENING WEBSOCKET')
         my_client = WebSocketClient(STOCKS_CLUSTER, KEY, entry_to_strats)
-        #run it asyncronously
+        # run it asyncronously
         my_client.run_async()
-        #subscribe to all stocks in the list
+        # subscribe to all stocks in the list
         print(':::::::::::::::::::SUBSCRIBING TO STOCKS IN WEBSOCKET')
         for ticker in websocket_symbols:
             my_client.subscribe(ticker)
-            #busy waiting loop that prevents the socket from closing
+            # busy waiting loop that prevents the socket from closing
         print(':::::::::::::::::::ENTERING BUSY WAITING WHILE DATA IS COLLECTED')
         timer = True
-        while( timer ):
-            #if 6 < hour < 12
+        while(timer):
+            # if 6 < hour < 12
             if(datetime.now().hour > 10 and datetime.now() < 12):
                 print(':::::::::::::::::::CLOSING WEBSOCKET')
                 my_client.close_connection()
     except:
         print('error log: Error In Phase 4 ------------------------------------------')
+
+
 if __name__ == "__main__":
-    #to run this using python main.py, remember to use this commaned in terminal before running: export PYTHONPATH="$PWD/src"
+    # to run this using python main.py, remember to use this commaned in terminal before running: export PYTHONPATH="$PWD/src"
     main()
-    
