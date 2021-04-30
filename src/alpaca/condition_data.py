b412da8315c977c734cf4a1a142d0920bf67f33f
Gonzalo <gnpantoja@outlook.com>
2021-04-29 23:47:11 -0500

    Fixed bug where take profit was subtracted by 2 percent instead of adding by 2 percent

--- src/alpaca/condition_data.py
+++ src/alpaca/condition_data.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
+
 def condition_data(dataframe):
     buy_list = []
     #alpaca_list = {'stock': [], 'stop_loss': [], 'stop_loss_limit': [], 'take_profit_limit': [], 'price': [], 'volume': [], 'score': []}
     for ticker in dataframe.index:
-        stop_loss = dataframe['Closing Price'][ticker] - dataframe['Closing Price'][ticker] * .0515
-        stop_loss_limit = dataframe['Closing Price'][ticker] - dataframe['Closing Price'][ticker] * .06
-        take_profit_limit = dataframe['Closing Price'][ticker] - dataframe['Closing Price'][ticker] * .02
+        stop_loss = dataframe['Closing Price'][ticker] - \
+            dataframe['Closing Price'][ticker] * .0515
+        stop_loss_limit = dataframe['Closing Price'][ticker] - \
+            dataframe['Closing Price'][ticker] * .06
+        take_profit_limit = dataframe['Closing Price'][ticker] + \
+            dataframe['Closing Price'][ticker] * .02
         buy_list.append(dict(
             symbol=str(dataframe['Stock'][ticker]),
-            take_profile_limit=str(round(take_profit_limit,3)),
-            stop_loss_price=str(round(stop_loss,3)),
-            stop_loss_price_limit=str(round(stop_loss_limit,3)),
+            take_profile_limit=str(round(take_profit_limit, 3)),
+            stop_loss_price=str(round(stop_loss, 3)),
+            stop_loss_price_limit=str(round(stop_loss_limit, 3)),
             price=str(dataframe['Closing Price'][ticker])
         ))
         print('buy_list data: ', buy_list)
@@ -23,5 +27,5 @@ def condition_data(dataframe):
         # alpaca_list['volume'].append(str(dataframe['Volume'][ticker]))
         # alpaca_list['score'].append(str(dataframe['Score'][ticker]))
         # alpaca_list['side'].append('buy')
-    #return pd.DataFrame(alpaca_list), buy_list
+    # return pd.DataFrame(alpaca_list), buy_list
     return buy_list
